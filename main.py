e98718cefe6dd0a38893e16014f8902de91d36db
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2024-10-09 20:18:36 +0300

    refactoring in .yml file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import aiohttp
